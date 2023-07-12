79af9b1db21b3b1596c7190c303d6fab70e72cd9
Theodule007 <theodulesaintil@gmail.com>
2023-07-11 22:58:18 -0400

    delocalisation des titres et des contenus avec yield-section

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>about Us | Hello city</title>
+    @section('title','About Us | Hello city')
+    
 
-
-    </head>
-    <body >
-       
+    @section('content')  
         <p> Build with &hearts; by Theodule Saintil.</p>
         <p> <a href="/home">return to homepage</a> </p>
 
-        <footer> 
-            <p> &copy; copyright {{ date('Y') }} </p>
-        </footer>
-    </body>
-</html>
+    @endsection
+
